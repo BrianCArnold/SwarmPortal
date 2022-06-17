0a6169a6951f4ea31bab1bb39a5ff922e7cdfdcb
Brian C. Arnold <connect@brian-arnold.dev>
2022-06-17 16:53:37 -0400

    Fixed issue with double-binding on config file.

--- SwarmPortal.Source.Docker/Configuration/DockerSourceConfiguration.cs
+++ SwarmPortal.Source.Docker/Configuration/DockerSourceConfiguration.cs
@@ -7,15 +7,14 @@ public class DockerSourceConfiguration : IDockerSourceConfiguration
     public static IDockerSourceConfiguration Create(IConfiguration config)
     {
         var dockerSourceConfig = new DockerSourceConfiguration();
-        config.Bind(dockerSourceConfig);
+        config.Bind(dockerSourceConfig, o => o.BindNonPublicProperties = false);
         return dockerSourceConfig;
     }
     private  DockerSourceConfiguration()
     {
     }
     public string DockerSocketUri { get; set; } = "unix:///var/run/docker.sock";
-    public List<string> SwarmPortalLabelPrefix { get; set; } = new List<string>{ "swarm","portal" };
+    public IEnumerable<string> SwarmPortalLabelPrefix { get; set; } = new []{ "swarm","portal" };
     public string NodeRole { get; set; } = "swarm.portal.node.role";
-    IEnumerable<string> IDockerSourceConfiguration.SwarmPortalLabelPrefix => SwarmPortalLabelPrefix;
 
 }
