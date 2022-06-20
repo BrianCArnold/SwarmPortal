e55a4cca03230a8131ebdb18e39923dfc7c04663
Brian C. Arnold <connect@brian-arnold.dev>
2022-06-19 22:04:42 -0400

    Guard not included in previous commit.

---
+++ SwarmPortal.FrontEnd/src/app/admin/guards/admin.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { HttpService } from '../../services/http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+
+  constructor(private http: HttpService){}
+  canActivate(
+    _: ActivatedRouteSnapshot,
+    __: RouterStateSnapshot): boolean {
+    return this.http.Identity == null ? false : this.http.Identity?.roles.includes('admin');
+  }
+
+}
